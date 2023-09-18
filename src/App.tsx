12cadf91d3ecf25a44f1903c36a1f947fa8f076c
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-18 12:26:37 +0900

    update: webRTC完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Peer, DataConnection } from "peerjs";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+type Msg = {
+  text: string;
+};
+
+type PeerConnection = {
+  id: string;
+  conn: DataConnection;
+};
+
+const GeneratePeerId = () => {
+  const rand = () => {
+    return Math.random().toString(36).substring(2);
+  };
+  return rand();
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [peer, setPeer] = useState<Peer | null>(null);
+  const [conn, setConn] = useState<PeerConnection | null>(null);
+  const [thisId, setThisId] = useState<string | null>(null);
+  const [inputId, setInputId] = useState<string>("");
+  const [inputMsg, setInputMsg] = useState<Msg>({ text: "" });
+  const [recievedMessage, setRecievedMessage] = useState<Msg | null>(null);
+
+  useEffect(() => {
+    const id = GeneratePeerId();
+    setThisId(id);
+    const peer = new Peer(id);
+    setPeer(peer);
+
+    peer.on("connection", (conn) => {
+      conn.on("data", (data) => {
+        const recieved: Msg = JSON.parse(data as string);
+        console.log(recieved);
+        setRecievedMessage(recieved);
+      });
+      conn.on("open", () => {
+        const res: Msg = {
+          text: `hello ${conn.peer}!  I am ${id}`,
+        };
+        conn.send(JSON.stringify(res));
+      });
+      setConn({ id: conn.peer, conn: conn });
+    });
+  }, []);
+
+  const connect = (toId: string) => {
+    const conn = peer!.connect(toId);
+    conn.on("data", (data) => {
+      const recieved: Msg = JSON.parse(data as string);
+      console.log(recieved);
+      setRecievedMessage({ text: recieved.text });
+    });
+    conn.on("open", () => {
+      const greet: Msg = {
+        text: `hi! I am ${thisId}`,
+      };
+      conn.send(JSON.stringify(greet));
+    });
+    setConn({ id: conn.peer, conn: conn });
+  };
+
+  const send = (msg: Msg) => {
+    conn!.conn.send(JSON.stringify(msg));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>I am {thisId}</div>
+      <br />
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <input
+          type="text"
+          value={inputId}
+          onChange={(e) => setInputId(e.target.value)}
+        />
+        <button onClick={() => connect(inputId)}>connect</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <br />
+      {conn && (
+        <>
+          <div>connected to {conn.id}</div>
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <input
+              type="text"
+              value={inputMsg.text}
+              onChange={(e) => setInputMsg({ text: e.target.value })}
+            />
+            <button onClick={() => send(inputMsg)}>send</button>
+          </div>
+        </>
+      )}
+      {recievedMessage && <div>recieved message: {recievedMessage!.text}</div>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
