c289802e19ca3e2d534b88478084d75a97eba580
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-19 12:10:29 +0900

    update: パーセントでカーソル実装できた

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Peer, DataConnection } from "peerjs";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 type XYZ = {
   x: number;
@@ -27,14 +27,19 @@ type = "sensorInfo" | "sensorInit"
 }
 */
 
-type sensorInfo = {
+// type PointerPosition = {
+//   x: number;
+//   y: number;
+// };
+
+type SensorPerInfo = {
   acc: XYZ;
   gyro: XYZ;
 };
 
 type Message = {
   type: "sensorInit" | "sensorInfo";
-  data: sensorInfo;
+  data: SensorPerInfo;
 };
 
 type Msg = {
@@ -46,17 +51,17 @@ type PeerConnection = {
   conn: DataConnection;
 };
 
-const GeneratePeerId = () => {
-  const rand = () => {
-    return Math.random().toString(36).substring(2);
-  };
-  return rand();
-};
+// const GeneratePeerId = () => {
+//   const rand = () => {
+//     return Math.random().toString(36).substring(2);
+//   };
+//   return rand();
+// };
 
 // radians to degrees
-const rad2deg = (rad: number) => {
-  return (rad * 180) / Math.PI;
-};
+// const rad2deg = (rad: number) => {
+//   return (rad * 180) / Math.PI;
+// };
 
 function App() {
   const [peer, setPeer] = useState<Peer | null>(null);
@@ -65,47 +70,36 @@ function App() {
   const [inputId, setInputId] = useState<string>("");
   const [inputMsg, setInputMsg] = useState<Msg>({ text: "" });
   const [recievedMessage, setRecievedMessage] = useState<Msg | null>(null);
-  const [sensorInfo, setSensorInfo] = useState<sensorInfo | null>(null);
-  const [initsensorInfo, setInitsensorInfo] = useState<sensorInfo>({
-    acc: { x: 0, y: 0, z: 0 },
-    gyro: { x: 0, y: 0, z: 0 },
-  });
+  const [sensorPerInfo, setSensorPerInfo] = useState<SensorPerInfo | null>(
+    null
+  );
+  // const [initSensorInfo, setInitSensorInfo] = useState<SensorPerInfo>({
+  //   acc: { x: 0, y: 0, z: 0 },
+  //   gyro: { x: 0, y: 0, z: 0 },
+  // });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const id = GeneratePeerId();
+    // const id = GeneratePeerId();
+    const id = "5q9dw3t2au6";
     setThisId(id);
     const peer = new Peer(id);
     setPeer(peer);
 
     peer.on("connection", (conn) => {
       conn.on("data", (data) => {
-        // log data type
-        console.log(typeof data);
-        console.log(data);
-
         // const recieved: Msg = data as Msg;
         // console.log(recieved);
         // console.log(recieved);
         // setRecievedMessage(recieved);
         const recieved: Message = data as Message;
-        console.log(recieved);
+        if (recieved.data.gyro.z > 0.02) console.log(recieved);
 
         if (recieved.type === "sensorInit") {
-          setInitsensorInfo(recieved.data);
+          // setInitSensorInfo(recieved.data);
+          console.log(recieved.data);
         } else if (recieved.type === "sensorInfo") {
-          setSensorInfo((prev) => {
-            if (!prev) return recieved.data;
-            else {
-              prev.gyro.x +=
-                recieved.data.gyro.x > 0.02 ? recieved.data.gyro.x : 0;
-              prev.gyro.y +=
-                recieved.data.gyro.y > 0.02 ? recieved.data.gyro.y : 0;
-              prev.gyro.z +=
-                recieved.data.gyro.z > 0.02 ? recieved.data.gyro.z : 0;
-              console.log(prev);
-              return prev;
-            }
-          });
+          setSensorPerInfo(recieved.data);
         }
       });
       conn.on("open", () => {
@@ -119,6 +113,30 @@ function App() {
       setConn({ id: conn.peer, conn: conn });
     });
   }, []);
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d");
+    if (!ctx) return;
+    const width = canvasRef.current!.width;
+    const height = canvasRef.current!.height;
+    ctx.clearRect(0, 0, width, height);
+    console.log(sensorPerInfo!.gyro.z);
+    console.log(sensorPerInfo!.gyro.x);
+    ctx.beginPath();
+    ctx.arc(
+      width - (width * sensorPerInfo!.gyro.z + width / 2),
+      height - (height * sensorPerInfo!.gyro.x + height / 2),
+      10,
+      0,
+      Math.PI * 2,
+      true
+    );
+    ctx.fillStyle = "lightskyblue";
+    ctx.fill();
+    ctx.strokeStyle = "deepskyblue";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+  }, [sensorPerInfo]);
 
   const connect = (toId: string) => {
     const conn = peer!.connect(toId);
@@ -142,8 +160,6 @@ function App() {
   const send = (msg: Msg) => {
     // Json to ArrayBuffer
     const ab = new TextEncoder().encode(JSON.stringify(msg)).buffer;
-    console.log(ab);
-
     conn!.conn.send(ab);
   };
 
@@ -182,29 +198,30 @@ function App() {
         </>
       )}
       {recievedMessage && <div>recieved message: {recievedMessage!.text}</div>}
-      {sensorInfo && (
-        <div
-          style={{
-            width: 5,
-            transform: `rotate(${
-              rad2deg(sensorInfo.gyro.z) - rad2deg(initsensorInfo.gyro.z)
-            }deg)`,
-          }}
-        >
+      {sensorPerInfo && (
+        <div>
           <div
             style={{
-              backgroundColor: "red",
               width: 5,
-              height: 80,
+              transform: `rotate(${sensorPerInfo.gyro.z * 180}deg)`,
             }}
-          ></div>
-          <div
-            style={{
-              backgroundColor: "blue",
-              width: 5,
-              height: 80,
-            }}
-          ></div>
+          >
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 5,
+                height: 80,
+              }}
+            ></div>
+            <div
+              style={{
+                backgroundColor: "blue",
+                width: 5,
+                height: 80,
+              }}
+            ></div>
+          </div>
+          <canvas width={600} height={600} ref={canvasRef}></canvas>
         </div>
       )}
     </>
